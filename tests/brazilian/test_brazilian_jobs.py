2e7c346883bc19a4966b404ef0f9a044a40c8e07
erikarg <erikatrue@gmail.com>
2023-02-17 12:49:34 -0300

    feat: add eleventh challenge

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,7 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    path = "tests/mocks/brazilians_jobs.csv"
+    file = read_brazilian_file(path)
+    assert "title" in file[0]
