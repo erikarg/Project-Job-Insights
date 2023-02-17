88a3c5fe90f732a0a93f33fbb92bf50b9979d667
erikarg <erikatrue@gmail.com>
2023-02-17 13:14:15 -0300

    fix: tenth challenge

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -4,4 +4,4 @@ from src.pre_built.counter import count_ocurrences
 def test_counter():
     path = "tests/mocks/jobs.csv"
     count = count_ocurrences(path, "end")
-    assert count == 5889
+    assert count == 3
